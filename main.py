f79aa618252be6e6d26316b048b2e863d4cd5bae
dmarchel2004 <33225488+dmarchel2004@users.noreply.github.com>
2017-11-10 13:59:02 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,12 @@
-#bibbo boi
+def main():
+    balance = 100
+    print "Welcome to the Python casino! What would you like to do?"
+    print "\n 1 - Guessing Game \n 2 - Craps \n 3 - In Between \n 4 - Directions \n 5 - Quit"
+    choice = 0
+    while choice != 5
+        choice = input("Enter 1-5 to choose what you want to do: ")
+        if choice = 1:
+            balance = guessingGame(balance)
+        elif choice = 2
+            balance =
+main()
