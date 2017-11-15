bd46d6302baa0a7a6765a2fa96e0c5a61873a5cf
dmarchel2004 <33225488+dmarchel2004@users.noreply.github.com>
2017-11-15 13:43:25 -0500

    Update thecasino.py

--- thecasino.py
+++ thecasino.py
@@ -1,73 +1,65 @@
+from random import randint
 
-import random
 def guessingGame(balance):
-    # This is a guess the number game.
-     import random
-
-     guessesTaken = 0
-
-     number = random.randint(1, 20)
-     print("I am thinking of a number between 1 and 20.")
-
-     while guessesTaken < 6:
-         print('Take a guess.')
-         guess = input()
-         guess = int(guess)
-
-         guessesTaken = guessesTaken + 1
-
-         if guess < number:
-             print('Your guess is too low.')
-
-         if guess > number:
-             print('Your guess is too high.')
-
-         if guess == number:
-             break
-
-     if guess == number:
-         guessesTaken = str(guessesTaken)
-         print('Good job, You guessed my number in ' + guessesTaken + ' guesses!')
-
-     if guess != number:
-         number = str(number)
-         print('Nope. The number I was thinking of was ' + number)
-
-
+    bet = input("Enter how much money you want to bet from $1-$100: ")
+    guessesTaken = 1
+    number = randint(1, 20)
+    print("I am thinking of a number between 1 and 20.")
+    while guessesTaken < 4:
+        guess = input('Take a guess:')
+        if guess < number:
+            print('Your guess is too low.')
+        if guess > number:
+            print('Your guess is too high.')
+        if guess == number:
+            guessesTaken = str(guessesTaken)
+            print('Good job, you guessed my number in ' + guessesTaken + ' guesses!')
+            print "You won", bet, "dollars."
+            balance = balance + bet
+            print "Your current balance is", balance
+            return balance
+        guessesTaken = guessesTaken + 1
+    if guessesTaken == 6:
+        print "You lost", bet, "dollars."
+        balance = balance - bet
+        print "Your current balance is", balance
+        return balance
 
 
 def craps(balance):
-    x = raw_input(' press "enter" to roll the dice')
-    die1 = random.randint(1,6)
-    die2 = random.randint(1,6)
-    dsum = die1 + die2
-    print "You rolled", dsum
+    bet = input("Enter how much money you want to bet from $1-$100: ")
+    balance = raw_input('Press "enter" to roll the dice')
+    dsum = 0
+    while dsum != 2 or dsum!= 3 or dsum !=12 or dsum != 7 or dsum != 11:
+        die1 = randint(1,6)
+        die2 = randint(1,6)
+        dsum = die1 + die2
+        print "You rolled", dsum
 
+        if dsum == 2 or dsum ==3 or dsum == 12:
+            print "You lose"
 
-    if dsum == 2 or dsum ==3 or dsum == 12:
-        game = "you lose"
+        elif dsum == 7 or dsum == 11:
+            print "You win"
 
-    elif dsum == 7 or dsum == 11:
-        game = "you win"
+        else:
 
-    else:
+            game = True
+            balance = raw_input('')
+            while game:
+        		die1 = random.randint(1,6)
+        		die2 = random.randint(1,6)
+        		dsum2 = die1 + die2
 
-        game = True
-        x = raw_input('')
-        while game:
-    		die1 = random.randint(1,6)
-    		die2 = random.randint(1,6)
-    		dsum2 = die1 + die2
+        		print "You rolled", dsum2
+        		balance = raw_input("")
 
-    		print "You rolled", dsum2
-    		x = raw_input("")
+        		if dsum2 == dsum:
+        	           print "you win"
 
-    		if dsum2 == dsum:
-    	           print "you win"
-
-    		elif dsum2 == 7:
-    	           print "you lose"
-    return balance
+        		elif dsum2 == 7:
+        	           print "you lose"
+        return balance
 
 
 
@@ -78,17 +70,21 @@ def inBetween(balance):
 
 def directions():
     print "\nGAMES: \n 1 - Guessing Game \n 2 - Craps \n 3 - In Between \n 4 - Help \n 5 - Quit"
-    print "HOW TO PLAY: \n \nGUESSING GAME: \nThe goal of the Guessing Game is pretty simple. You are to guess a number between 1 and 20. You will continue to \nguess to you reach the number. Your pay out is determined by how many tries you take to guess the number.\n"
+    print "\n HOW TO PLAY: "
+    print "GUESSING GAME: \n The goal of the Guessing Game is pretty simple. You are to guess a number between 1 and 20. You will continue to \nguess to you reach the number. Your pay out is determined by how many tries you take to guess the number.\n"
     print "CRAPS: \n Coming Soon!\n"
     print "IN BETWEEN: \n Coming Soon!\n"
 
 
 def main():
-    balance = "100"
+    balance = 100
     print "Welcome to the Python casino! What would you like to do?"
     choice = "0"
     while choice != "5":
-        choice = raw_input("\n 1 - Guessing Game \n 2 - Craps \n 3 - In Between \n 4 - Help \n 5 - Quit \nEnter 1-5 to choose what you want to do: ")
+        if balance == 0:
+            print "You lost all your money."
+            return
+        choice = raw_input("\n 1 - Guessing Game \n 2 - Craps \n 3 - In Between \n 4 - Help \n 5 - Quit \n \n Enter 1-5 to choose what you want to do: ")
         if choice == "1":
             balance = guessingGame(balance)
         elif choice == "2":
@@ -96,6 +92,9 @@ def main():
         elif choice == "3":
             balance = inBetween(balance)
         elif choice == "4":
-            help()
+            directions()
+        elif choice == "5":
+            print "Your final balance is", balance
+            return
 
 main()
