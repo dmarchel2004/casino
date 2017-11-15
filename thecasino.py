46ac7a45b1511a7d8d770f2b3acab43601bc7721
dmarchel2004 <33225488+dmarchel2004@users.noreply.github.com>
2017-11-15 17:12:28 -0500

    Update thecasino.py

--- thecasino.py
+++ thecasino.py
@@ -1,12 +1,12 @@
 from random import randint
 
 def guessingGame(balance):
-    bet = input("Enter how much money you want to bet from $1-$100: ")
+    bet = input("Enter how much money you want to bet from $1 - $" + str(balance) + ": ")
     guessesTaken = 1
     number = randint(1, 20)
     print("I am thinking of a number between 1 and 20.")
-    while guessesTaken < 4:
-        guess = input('Take a guess:')
+    while guessesTaken < 5:
+        guess = input('Take a guess: ')
         if guess < number:
             print('Your guess is too low.')
         if guess > number:
@@ -19,54 +19,48 @@ def guessingGame(balance):
             print "Your current balance is", balance
             return balance
         guessesTaken = guessesTaken + 1
-    if guessesTaken == 6:
+    if guessesTaken == 5:
+        print "The number was", number
         print "You lost", bet, "dollars."
         balance = balance - bet
         print "Your current balance is", balance
         return balance
 
-
 def craps(balance):
-    bet = input("Enter how much money you want to bet from $1-$100: ")
-    balance = raw_input('Press "enter" to roll the dice')
-    dsum = 0
-    while dsum != 2 or dsum!= 3 or dsum !=12 or dsum != 7 or dsum != 11:
-        die1 = randint(1,6)
-        die2 = randint(1,6)
-        dsum = die1 + die2
+    bet = input("Enter how much money you want to bet from $1 - $" + str(balance) + ": ")
+    start = raw_input('Press "enter" to roll the dice: ')
+    die1 = randint(1,6)
+    die2 = randint(1,6)
+    dsum = die1 + die2
+    dsumFirst = dsum
+    running = True
+    tries = 1
+    while running:
+        print "First is", dsumFirst
         print "You rolled", dsum
-
-        if dsum == 2 or dsum ==3 or dsum == 12:
-            print "You lose"
-
-        elif dsum == 7 or dsum == 11:
-            print "You win"
-
+        if dsum != 2 and 3 and 12 and 7 and 11:
+            if tries > 1 and dsum == dsumFirst:
+                balance = balance + bet
+                print "You won", bet, "dollars. \n Your current balance is", balance
+                return balance
+            else:
+                print "You didn't win or lose."
+                start = raw_input('Press "enter" to roll the dice: ')
+                die1 = randint(1,6)
+                die2 = randint(1,6)
+                dsum = die1 + die2
+                tries += 1
+        elif dsum != 2 and 3 and 12:
+            balance = balance + bet
+            print "You won", bet, "dollars. \n Your current balance is", balance
+            return balance
         else:
-
-            game = True
-            balance = raw_input('')
-            while game:
-        		die1 = random.randint(1,6)
-        		die2 = random.randint(1,6)
-        		dsum2 = die1 + die2
-
-        		print "You rolled", dsum2
-        		balance = raw_input("")
-
-        		if dsum2 == dsum:
-        	           print "you win"
-
-        		elif dsum2 == 7:
-        	           print "you lose"
-        return balance
-
-
-
+            balance = balance - bet
+            print "You lost", bet, "dollars. \n Your current balance is", balance
+            return balance
 
 def inBetween(balance):
     print("Coming Soon")
-
 
 def directions():
     print "\nGAMES: \n 1 - Guessing Game \n 2 - Craps \n 3 - In Between \n 4 - Help \n 5 - Quit"
@@ -74,7 +68,6 @@ def directions():
     print "GUESSING GAME: \n The goal of the Guessing Game is pretty simple. You are to guess a number between 1 and 20. You will continue to \nguess to you reach the number. Your pay out is determined by how many tries you take to guess the number.\n"
     print "CRAPS: \n Coming Soon!\n"
     print "IN BETWEEN: \n Coming Soon!\n"
-
 
 def main():
     balance = 100
