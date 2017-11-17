8c99bbea380a3f49bb818c1d2fe31b8ce772e947
dmarchel2004 <33225488+dmarchel2004@users.noreply.github.com>
2017-11-17 15:01:04 -0500

    Update thecasino.py

--- thecasino.py
+++ thecasino.py
@@ -1,33 +1,35 @@
 from random import randint
 
 def guessingGame(balance):
-    bet = input("Enter how much money you want to bet from $1 - $" + str(balance) + ": ")
+    bet = betty(balance)
     guessesTaken = 1
     number = randint(1, 20)
     print("I am thinking of a number between 1 and 20.")
     while guessesTaken < 5:
-        guess = input('Take a guess: ')
-        if guess < number:
-            print('Your guess is too low.')
-        if guess > number:
-            print('Your guess is too high.')
-        if guess == number:
-            guessesTaken = str(guessesTaken)
-            print('Good job, you guessed my number in ' + guessesTaken + ' guesses!')
-            print "You won", bet, "dollars."
-            balance = balance + bet
-            print "Your current balance is", balance
+        guess = raw_input('Take a guess: ')
+        if guess.isdigit():
+            guess = int(guess)
+            if guess < number:
+                print('Your guess is too low.')
+            if guess > number:
+                print('Your guess is too high.')
+            if guess == number:
+                guessesTaken = str(guessesTaken)
+                print('Good job, you guessed my number in ' + guessesTaken + ' guesses!')
+                print "You won", bet, "dollars."
+                balance = balance + bet
+                print "Your current balance is", balance
+                return balance
+            guessesTaken = guessesTaken + 1
+        if guessesTaken == 5:
+            print "The number was", number
+            print "You lost", bet, "dollars."
+            balance = balance - bet
+            print "Your current balance is $",balance
             return balance
-        guessesTaken = guessesTaken + 1
-    if guessesTaken == 5:
-        print "The number was", number
-        print "You lost", bet, "dollars."
-        balance = balance - bet
-        print "Your current balance is", balance
-        return balance
 
 def craps(balance):
-    bet = input("Enter how much money you want to bet from $1 - $" + str(balance) + ": ")
+    bet = betty(balance)
     start = raw_input('Press "enter" to roll the dice: ')
     die1 = randint(1,6)
     die2 = randint(1,6)
@@ -41,7 +43,7 @@ def craps(balance):
         if dsum != 2 and 3 and 12 and 7 and 11:
             if tries > 1 and dsum == dsumFirst:
                 balance = balance + bet
-                print "You won", bet, "dollars. \n Your current balance is", balance
+                print "You won", bet, "dollars. \n Your current balance is $",balance
                 return balance
             else:
                 print "You didn't win or lose."
@@ -52,11 +54,11 @@ def craps(balance):
                 tries += 1
         elif dsum != 2 and 3 and 12:
             balance = balance + bet
-            print "You won", bet, "dollars. \n Your current balance is", balance
+            print "You won", bet, "dollars. \n Your current balance is $",balance
             return balance
         else:
             balance = balance - bet
-            print "You lost", bet, "dollars. \n Your current balance is", balance
+            print "You lost", bet, "dollars. \n Your current balance is $",balance
             return balance
 
 def inBetween(balance):
@@ -69,8 +71,22 @@ def directions():
     print "CRAPS: \n Coming Soon!\n"
     print "IN BETWEEN: \n Coming Soon!\n"
 
+def betty(balance):
+    validBet = 0
+    while validBet == 0:
+        print "You have $", balance
+        bet = raw_input("Enter a bet: ")
+        if bet.isdigit():
+            bet = int(bet)
+            if bet <= balance and bet > 0:
+                if bet == 666:
+                    print "Just when you think its over. Your Demons laugh and whisper. /\/\(=-=)/\/\ "
+                return bet
+        else:
+            print "That is not a valed bet."
+
 def main():
-    balance = 100
+    balance = 1000
     print "Welcome to the Python casino! What would you like to do?"
     choice = "0"
     while choice != "5":
@@ -91,3 +107,4 @@ def main():
             return
 
 main()
+
